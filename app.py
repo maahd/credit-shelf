1538e3b447df1b1da45d49eb0e3cf12ee7819275
maahd <maahdsh@gmail.com>
2020-11-01 05:34:28 +0100

    moves api key to google .env file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify, request, render_template
 from flask_googlemaps import GoogleMaps
 
+# for env variables
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 app = Flask(__name__, template_folder='website')
 
 # you can set key as config
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyAwOC5SGtMDCwI854eq7dQwcqMKSxOZb_Y"
+app.config['GOOGLEMAPS_KEY'] = os.getenv("googleMapsApiKey")
 
 # Initialize the extension
 GoogleMaps(app)
